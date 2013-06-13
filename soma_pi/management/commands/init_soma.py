32354b78d287db17a3c53b299e67a15236b55704
fredley <fredley@gmail.com>
2013-06-13 21:04:12 +0000

    Fixed the way stream urls are grabbed

--- soma_pi/management/commands/init_soma.py
+++ soma_pi/management/commands/init_soma.py
@@ -1,4 +1,5 @@
 import urllib2
+import logging
 
 import xml.etree.ElementTree as ET
 from optparse import make_option
@@ -6,6 +7,8 @@ from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
 
 from soma_pi.models import Station
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'Initializes the database with all SomaFM stations'
@@ -36,10 +39,21 @@ class Command(BaseCommand):
             return
         self.stdout.write('Parsed data')
         for channel in data.findall('channel'):
+            try:
+                pls = urllib2.urlopen(channel.find('fastpls').text).read()
+                for line in iter(pls.splitlines()):
+                    if line[0:5] == 'File1':
+                        stream = line[6:]
+                        break
+                url = stream # Just to dump us out if stream is not found
+            except:
+                self.stdout.write('Problem getting url for station: %s' % channel.find('title').text)
+                continue
+
             new_station = Station()
             new_station.name = channel.find('title').text
             new_station.description = channel.find('description').text
-            new_station.url = channel.find('fastpls').text
+            new_station.url = stream
             new_station.image = channel.find('image').text
             new_station.save()
             self.stdout.write('Added station: %s' % new_station.name)
