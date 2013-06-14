f7aea3abadef869c0e2b067e426950497469e3ea
fredley <fredley@gmail.com>
2013-06-14 20:07:38 +0000

    Increase sizes of text fields

--- soma_pi/models.py
+++ soma_pi/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Station(models.Model):
     """ Keeps track of the station URL and details """
 
-    url = models.CharField(max_length=60)
+    url = models.CharField(max_length=300)
     image = models.CharField(max_length=200)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
